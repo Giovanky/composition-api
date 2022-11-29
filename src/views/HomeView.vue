e95ce9d35bf47865e23200329994ed45473ad35b
GiovannyGarzon <giovankyandres@gmail.com>
2022-11-29 15:45:40 -0300

    updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Reactive vs Ref</h1>
+
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+
+    <button @click="changeIronman">changeIronman</button>
+    <button @click="changeHulk">changeHulk</button>
+
+    <h3>Ironman: {{ ironmanIsAlive }}</h3>
+    <h3>Hulk: {{ hulkIsAlive }}</h3>
+
+    <button @click="changeStatus"> changeStatus</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive } from 'vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const ironman = ref({name: 'Tony', age: 50})
+    const hulk = reactive({name: 'Bruce', age: 50})
+
+    const ironmanIsAlive = ref(true)
+    const hulkIsAlive = ref(true)
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive.value 
+      ironmanIsAlive = !ironmanIsAlive
+    }
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+      hulkIsAlive,
+
+      changeStatus,
+      changeIronman: () => {
+        ironman.value.name = 'Tony Stark',
+        ironman.value.age = 55
+      },
+      changeHulk: () => {
+        hulk.name = 'Bruce Banner',
+        hulk.age = 45
+      }
+    }
   }
 }
 </script>
